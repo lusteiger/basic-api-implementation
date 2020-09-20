e1957c0da4f3b7b34aa2cd3cce2d42b2ba383c11
lusteiger <leonardoxy7@gmail.com>
2020-09-20 11:10:30 +0800

    [xinyu lu] add test property

--- src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalTime;
@@ -30,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class VoteControllerTest {
 
     @Autowired
